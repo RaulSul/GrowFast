9dcc36ffc325105228c75366823072090b0a3d61
RaulSul <raul.sulaimanov@gmail.com>
2019-06-20 11:59:45 +0200

    added iOS project

---
+++ mobile/iOS/GrowFast/GrowFast/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrowFast
+//
+//  Created by Raul (Private) on 20.06.19.
+//  Copyright © 2019 Raul Sulaimanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
